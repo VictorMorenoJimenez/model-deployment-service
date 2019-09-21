0bb25bbf669e0efde0e3dde5c815121295331e37
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-09-21 20:22:40 +0200

    Add empty route controllers

---
+++ src/api/routes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/model/:id', (req, res) => {
+});
+
+router.post('/model', (req, res) => {
+});
+
+router.put('/model/:id', (req, res) => {
+});
+
+router.delete('/model/:id', (req, res) => {
+});
+
+router.post('/predict/:id', (req, res) => {
+});
+
+router.post('/evaluate/:id', (req, res) => {
+});
+
+export default router;
