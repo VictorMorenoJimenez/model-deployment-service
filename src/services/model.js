b8aa55efa05c69457ed52aba3d2d6e72b2d15eff
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-09-22 22:16:06 +0200

    Remove storage service and move its functionality to Model service

---
+++ src/services/model.js
@@ -0,0 +1,48 @@
+import * as R from 'ramda';
+
+import { getModelMetadata } from './metadata';
+import { Model } from '../models';
+import { convertToObject } from '../utils/db';
+
+const saveModel = (storageProvider) => async (modelFile) => {
+  const metadata = getModelMetadata(modelFile);
+  const modelDoc = new Model(metadata);
+  const model = await modelDoc.save();
+  await storageProvider.uploadFile(modelFile, model._id);
+  return model._id;
+};
+
+const updateModel = (storageProvider) => async (modelFile, modelId) => {
+  const metadata = getModelMetadata(modelFile);
+  await Model.findByIdAndUpdate(modelId, metadata).exec();
+  await storageProvider.uploadFile(modelFile, modelId);
+  return modelId;
+};
+
+const retreiveModel = (storageProvider) => async (id) => {
+  const model = await Model.findById(id).exec();
+  return storageProvider.downloadFile(model.resource);
+};
+
+const deleteModel = (storageProvider) => async (id) => {
+  const model = await Model.findByIdAndDelete(id).exec();
+  return storageProvider.deleteFile(model.resource);
+};
+
+const getAllModels = async () => {
+  const models = await Model.find({}).exec();
+  const pairs = models.map((model) => {
+    const obj = convertToObject(model);
+    return [obj.id, obj];
+  });
+  return R.fromPairs(pairs);
+};
+
+const getModel = async (modelId) => {
+  const model = await Model.findById(modelId).exec();
+  return convertToObject(model);
+};
+
+export {
+  saveModel, retreiveModel, deleteModel, updateModel, getAllModels, getModel,
+};
