eab4cdbafdb3dbaf77df53e0ce45bd8f21fa64b6
Antonio Molner Domenech <antoniomdk@hotmail.es>
2019-09-22 14:42:31 +0200

    Add server start up and http handling

--- src/server.js
+++ src/server.js
@@ -1,1 +1,52 @@
-import './config/env';
+import http from 'http';
+import app from './api';
+import config from './config';
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+};
+
+const port = normalizePort(config.PORT);
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
+};
+
+
+server.on('error', onError);
+server.on('listening', onListening);
